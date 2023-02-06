11b000fc9257c803e9f3296c86dc5ffa0ac977d8
kristina <kriskolu@yandex.ru>
2023-02-06 19:22:14 +0700

    калькулятор

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Input a srecond number: ");
+        int num2 = in.nextInt();
+        int operSum = num1 + num2;
+        int operMin = num1-num2;
+        int operUmn = num1*num2;
+        double operDel = num1/num2;
+        int operOst = num1%num2;
+        int operSt=num1 ;
+        for(int i = 1; i<num2; i++){
+            operSt =operSt*num1;
+        }
+        System.out.println(operSum);
+        System.out.println(operMin);
+        System.out.println(operUmn);
+        System.out.println(operDel);
+        System.out.println(operOst);
+        System.out.println(operSt);
+
     }
 }
